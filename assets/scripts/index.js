12c13d79c7120d2a77ed843d7eb78641171889b6
aubreezy0 <aubreezy0@gmail.com>
2018-05-10 17:04:14 -0400

    adds handlebar files for forms
    adds handlebars files for auth forms Sign In, Sign Up, Change Password,
    and Sign Out. Adds handlebars forms for CRUD user files, Create, Add,
    Update, and Delete.
    Removes irrelevant HTML from index.html.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,9 +5,45 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
 const authEvents = require('./auth/events')
 const fileUploadEvents = require('./fileupload/fileuploadevents')
+
+const signUpUserHandlebars = require('./templates/sign-up.handlebars')
+const signUpUserHTML = signUpUserHandlebars()
+
+const signInUserHandlebars = require('./templates/sign-in.handlebars')
+const signInUserHTML = signInUserHandlebars()
+
+const changePasswordUserHandlebars = require('./templates/change-password.handlebars')
+const changePasswordUserHTML = changePasswordUserHandlebars()
+
+const signOutUserHandlebars = require('./templates/sign-out.handlebars')
+const signOutUserHTML = signOutUserHandlebars()
+
+const createFileUploadHandlebars = require('./templates/file-upload/create-file.handlebars')
+const createFileUploadHTML = createFileUploadHandlebars()
+
+const updateFileUploadHandlebars = require('./templates/file-upload/update-file.handlebars')
+const updateFileUploadHTML = updateFileUploadHandlebars()
+
+const viewFileUploadHandlebars = require('./templates/file-upload/view-file.handlebars')
+const viewFileUploadHTML = viewFileUploadHandlebars()
+
+const deleteFileUploadHandlebars = require('./templates/file-upload/delete-file.handlebars')
+const deleteFileUploadHTML = deleteFileUploadHandlebars()
+
 $(() => {
+  // your JS code goes here
   authEvents.addHandlers()
   fileUploadEvents.addHandlers()
+  $('body').append(signUpUserHTML)
+  $('body').append(signInUserHTML)
+  $('body').append(changePasswordUserHTML)
+  $('body').append(signOutUserHTML)
+  $('body').append(createFileUploadHTML)
+  $('body').append(updateFileUploadHTML)
+  $('body').append(viewFileUploadHTML)
+  $('body').append(deleteFileUploadHTML)
+
 })
