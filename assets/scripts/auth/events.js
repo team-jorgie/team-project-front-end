617e89de2d923d5a7a584d5bce5ccd99fabbac2c
dylan-jannetty <djannetty29@gmail.com>
2018-05-10 11:42:06 -0400

    Auth is complete
    events.js:
      added proper .then and .catch that for each onSignUp, onSignIn, onSignOut,
      and onChangePassword.
      added addHandlers so each event is linked to the forms.
    
    ui.js:
      added success and failure messages for each success and failure of each
      form. (Temporary)
    
    index.js:
      required auth/events file and placed in a variable called authEvents.
      added authEvents.addHandlers too the anonymous function to link the forms
      with each event.
    
    index.html:
      added a username section in the sign up form.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,35 +6,40 @@ const api = require('./api')
 const onSignUp = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
+  console.log(data)
   api.signUp(data)
-    .then(ui.)
-    .catch(ui.)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
 }
 
 const onSignIn = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.signIn(data)
-    .then(ui.)
-    .catch(ui.)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
 }
 
 const onChangePassword = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.changePassword(data)
-    .then(ui.)
-    .catch(ui.)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
 }
 
 const onSignOut = function (event) {
   event.preventDefault()
   api.signOut()
-    .then(ui.)
-    .catch(ui.)
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
 }
 
 const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
 }
 
 module.exports = {
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,6 +2,9 @@
 const store = require('../store')
 
 const signUpSuccess = function () {
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed up</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
@@ -9,6 +12,9 @@ const signUpSuccess = function () {
 }
 
 const signUpFailure = function () {
+  $('#message').html(`<div class="alert alert-danger" role="alert">You have failed to sign in!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
@@ -16,16 +22,20 @@ const signUpFailure = function () {
 }
 
 const signInSuccess = function (data) {
-
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed in!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
   )
   store.user = data.user
-
 }
 
 const signInFailure = function () {
+  $('#message').html(`<div class="alert alert-danger" role="alert">You have failed sign in!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
@@ -33,6 +43,9 @@ const signInFailure = function () {
 }
 
 const changePasswordSuccess = function () {
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully changed your password!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
@@ -40,18 +53,27 @@ const changePasswordSuccess = function () {
 }
 
 const changePasswordFailure = function () {
+  $('#message').html(`<div class="alert alert-danger" role="alert">Failed to change password</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
   )
 }
 const signOutSuccess = function () {
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed out!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
   )
 }
 const signOutFailure = function () {
+  $('#message').html(`<div class="alert alert-danger" role="alert">Failed to sign out!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
