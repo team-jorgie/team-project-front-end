9f984900a11a9d1f1e0ada5ea6bf45b772879b0d
omniczech <mikepetschek@gmail.com>
2018-05-12 12:06:21 -0400

    Adds more styling to GET results
    Results now go through a forEach loop to sort your files into a separate
    array which is put in using the same format but with a currently
    non-functional form for deleting the file.

--- assets/scripts/fileupload/fileuploadui.js
+++ assets/scripts/fileupload/fileuploadui.js
@@ -1,9 +1,10 @@
 'use strict'
-// const store = require('../store')
+const store = require('../store')
+const moment = require('moment')
 
 const createFileUploadSuccess = function (data) {
   console.log(data)
-  $('#message').html(`<div class="alert alert-success" role="alert"><p><a href="${data.fileupload.url}" download="${data.fileupload.title}">Name: ${data.fileupload.title}</a></p><p>Owner: ${data.fileupload.owner}</p><p>Created: ${data.fileupload.createdAt}</p><p>Updated: ${data.fileupload.updatedAt}</p></div>`)
+  $('#message').html(`<div class="alert alert-success" role="alert"><p><a href="${data.fileupload.url}" download="${data.fileupload.title}">Name: ${data.fileupload.title}</a></p><p>Owner: ${data.fileupload.owner}</p><p>Created: ${data.fileupload.createdAt}</p><p>Updated: ${data.fileupload.updatedAt}</p><form><input type="submit" value="Delete"></form></div>`)
   $('.all-files').append(`<p><a href="${data.fileupload.url}" download="${data.fileupload.title}">Name: ${data.fileupload.title}</a></p><p>Owner: ${data.fileupload.owner}</p><p>Created: ${data.fileupload.createdAt}</p><p>Updated: ${data.fileupload.updatedAt}</p>`)
   $('#message').css('text-align', 'center')
   $('form').trigger('reset')
@@ -34,7 +35,9 @@ const updateFileUploadFailure = function () {
   )
 }
 
-const deleteFileUploadSuccess = function () {
+const deleteFileUploadSuccess = function (data) {
+  console.log(data)
+  $('#message').html('You successfully deleted your file!')
   setTimeout(() => {
     $('#message').html('')
   }, 3000
@@ -50,12 +53,25 @@ const deleteFileUploadFailure = function () {
 
 const getFileUploadSuccess = function (data) {
   console.log(data)
+  $('.my-files').remove()
   $('.all-files').remove()
-  let resultsHtml = '<div class="all-files">'
+  const myFiles = []
+  data.uploads.forEach((file) => {
+    if (file.owner === store.user._id) {
+      myFiles.push(file)
+    }
+  })
+  console.log(myFiles)
+  let myResultsHtml = '<div class="my-files col-md-6"><h3>My Files</h3>'
+  myFiles.forEach((result) => {
+    myResultsHtml = myResultsHtml + `<p><a href="${result.url}" download="${result.title}">Name: ${result.title}</a></p><p>Owner: ${result.owner}</p><p>Created: ${result.createdAt}</p><p>Updated: ${result.updatedAt}</p><p>Tags: ${result.tag}</p><form><input type="submit" value="Delete"></form>`
+  })
+  let resultsHtml = '<div class="all-files col-md-6"><h3>All Files</h3>'
   data.uploads.forEach((result) => {
-    resultsHtml = resultsHtml + `<p><a href="${result.url}" download="${result.title}">Name: ${result.title}</a></p><p>Owner: ${result.owner}</p><p>Created: ${result.createdAt}</p><p>Updated: ${result.updatedAt}</p>`
+    resultsHtml = resultsHtml + `<p><a href="${result.url}" download="${result.title}">Name: ${result.title}</a></p><p>Owner: ${result.owner}</p><p>Created: ${result.createdAt}</p><p>Updated: ${result.updatedAt}</p><p>Tags: ${result.tag}</p>`
   })
   resultsHtml = resultsHtml + '</div>'
+  $('form.view-file').append(myResultsHtml)
   $('form.view-file').append(resultsHtml)
   setTimeout(() => {
     $('#message').html('')
